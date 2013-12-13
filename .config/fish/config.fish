6ce128b2dc79ff527ff2587b50dead8adc693820
Simon Goumaz <simon@attentif.ch>
2013-12-13 17:43:35 +0100

    Updated Node.js version to 0.10.23

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set -x PATH "/usr/local/Cellar/ruby/2.0.0-p0/bin" "/usr/local/opt/ruby/bin" $PAT
 set -x NODE_PATH "/usr/local/lib/node" "/usr/local/lib/node_modules" $NODE_PATH
 
 # set paths for nvm
-set NVM_NODEVER v0.8.23
+set NVM_NODEVER v0.10.23
 set -x PATH ~/.nvm/$NVM_NODEVER/bin $PATH
 set -x MANPATH ~/.nvm/$NVM_NODEVER/share/man $MANPATH
 
