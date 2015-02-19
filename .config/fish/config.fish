7a512e196d63f0651fcb4a033cd005f01d726d2f
Simon Goumaz <simon@attentif.ch>
2015-02-19 17:29:17 +0100

    Updated default node version to 0.12.0

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set -x PATH "/usr/local/Cellar/ruby/2.0.0-p0/bin" "/usr/local/opt/ruby/bin" $PAT
 set -x NODE_PATH "/usr/local/lib/node" "/usr/local/lib/node_modules" $NODE_PATH
 
 # node (current version, via n)
-n 0.10.33
+n 0.12.0
 
 # expose PATH to graphical apps
 launchctl setenv PATH $PATH
