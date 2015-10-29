766ea4a44d2e02c1dd5b8f0568b395a09b2091f1
Simon Goumaz <simon@attentif.ch>
2015-10-29 11:13:01 +0800

    Cleaned up Homebrew packages

--- brew.bash
+++ brew.bash
@@ -7,26 +7,26 @@ brew update
 brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+# brew install coreutils
+# echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
 
 # Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-brew tap josegonzalez/homebrew-php
-brew install php54
-brew install ruby
+# brew tap homebrew/dupes
+# brew install homebrew/dupes/grep
+# brew tap josegonzalez/homebrew-php
+# brew install php54
+# brew install ruby
 
 # Install everything else
 brew install ag
-brew install ack
 brew install git
 brew install git-extras
+brew install httpie
 brew install ffmpeg
 brew install graphicsmagick
 brew install htop
 brew install mobile-shell
-brew install mysql
+# brew install mysql
 
 # Remove outdated versions from the cellar
 brew cleanup
