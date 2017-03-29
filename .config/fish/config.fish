b2c7e0157332744917cf034ae7b89ec7009357c4
Simon Goumaz <simon@attentif.ch>
2017-03-29 14:02:23 +0800

    Removed obsolete Ruby/Gems path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,6 @@
 # Homebrew
 set -x PATH ""(brew --prefix)"/bin" $PATH
 
-# use latest Ruby + Gems
-set -x PATH "/usr/local/Cellar/ruby/2.0.0-p0/bin" "/usr/local/opt/ruby/bin" $PATH
-
 # node (default)
 set -x NODE_PATH "/usr/local/lib/node" "/usr/local/lib/node_modules" $NODE_PATH
 
