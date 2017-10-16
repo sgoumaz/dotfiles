2f24cb99886a613a107a70a29f08153da2e3d044
Simon Goumaz <simon@attentif.ch>
2017-10-16 17:47:32 +0800

    Added fd to toolset

--- brew.bash
+++ brew.bash
@@ -6,19 +6,9 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
-# Install GNU core utilities (those that come with OS X are outdated)
-# brew install coreutils
-# echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
-
-# Install more recent versions of some OS X tools
-# brew tap homebrew/dupes
-# brew install homebrew/dupes/grep
-# brew tap josegonzalez/homebrew-php
-# brew install php54
-# brew install ruby
-
 # Install everything else
 brew install ag
+brew install fd
 brew install git
 brew install git-extras
 brew install gpg2
