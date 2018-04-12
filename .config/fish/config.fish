def7663cd25c17ce206d2c78c8865ece1f038d0c
Simon Goumaz <simon@attentif.ch>
2018-04-12 11:37:33 +0800

    Added Rust

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 # Homebrew
 set -x PATH ""(brew --prefix)"/bin" $PATH
 
+# Rust
+set -x PATH $HOME"/.cargo/bin" $PATH
+
 # node (default)
 set -x NODE_PATH "/usr/local/lib/node" "/usr/local/lib/node_modules" $NODE_PATH
 
