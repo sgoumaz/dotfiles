3b7be5523e88269e9e8abd77cedcbefc559b19a9
Simon Goumaz <simon@attentif.ch>
2020-12-15 11:05:32 +0100

    Updated git config, Slate focused apps, npm

--- npm.bash
+++ npm.bash
@@ -5,10 +5,9 @@
 npm update -g
 
 npm install n -g
-npm install jshint -g
 npm install gh -g
 npm install grunt-cli -g
 npm install live-server -g
 npm install npm-check-updates -g
-npm install plato -g
+npm install semistandard -g
 npm install trash-cli -g
