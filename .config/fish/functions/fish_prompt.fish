c59a15bbb472a1cc9c25e23388fd43c7e4b3fdaa
Simon Goumaz <simon@attentif.ch>
2024-08-12 12:19:13 +0200

    Fish prompt: no longer need to check for Warp

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -63,12 +63,10 @@ function fish_prompt --description 'Write out the prompt'
   set_color normal
   prompt_git_status
 
-  if test $TERM_PROGRAM != "WarpTerminal"
-    echo
-    if not test $last_status -eq 0
-      set_color $fish_color_error
-    end
-    echo -n '▸ '
-    set_color normal
+  echo
+  if not test $last_status -eq 0
+    set_color $fish_color_error
   end
+  echo -n '▸ '
+  set_color normal
 end
