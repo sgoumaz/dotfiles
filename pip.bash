e4811b13a5e2a42f5c25498f0b5ea70f2bcd04d0
Simon Goumaz <simon@attentif.ch>
2025-11-04 08:51:12 +0100

    Beets: add `fromfilename` plugin

--- pip.bash
+++ pip.bash
@@ -3,4 +3,4 @@
 pip install beets
 # shouldn't be necessary apparently, but still had to reinstall this:
 # pip install certifi
-pip install "beets[fish,convert,musicbrainz,discogs,fetchart,lyrics]"
+pip install "beets[fish,convert,fromfilename,musicbrainz,discogs,fetchart,lyrics]"
